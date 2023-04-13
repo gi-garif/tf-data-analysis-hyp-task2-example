bee73e3ed2ffd2b0d5c4c2e14f3b29bbf3710945
gi-garif <84095383+gi-garif@users.noreply.github.com>
2023-04-13 11:15:13 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -10,25 +10,10 @@ def solution(x: np.array, y: np.array) -> bool:
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы    
 
-    # загрузка выборок
-    hist_data = pd.read_csv("/historical_data.csv")
-    test_data_1 = pd.read_csv("/modified_data_of_type_1.csv")
-    test_data_2 = pd.read_csv("/modified_data_of_type_2.csv")
-    test_data_3 = pd.read_csv("/modified_data_of_type_3.csv")
-
-    hist = hist_data.iloc[0].to_list()
-    test1 = test_data_1.iloc[0].to_list()
-    test2 = test_data_2.iloc[0].to_list()
-    test3 = test_data_3.iloc[0].to_list()
+    stat, pvalue = ks_2samp(x, y)
     
-    # расчет статистики критерия и p-value для каждой пары выборок
-    stat_1, pvalue_1 = ks_2samp(hist, test1)
-    stat_2, pvalue_2 = ks_2samp(hist, test2)
-    stat_3, pvalue_3 = ks_2samp(hist, test3)
-
-    # проверка гипотезы однородности на уровне значимости 0.04
     alpha = 0.04
-    if pvalue_1 < alpha or pvalue_2 < alpha or pvalue_3 < alpha:
+    if pvalue < alpha:
         result = True
     else:
         result = False
